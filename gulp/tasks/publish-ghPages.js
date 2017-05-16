a6c2ab39b5473e5ee41ed7534bd9737c9d2bae33
Mukherjee <pmukherj@4dempmukherj.ad.here.com>
2017-05-16 11:13:08 +0200

    solved the fa issue by simply providing a relative URL, fitting for all deploys

--- gulp/tasks/publish-ghPages.js
+++ gulp/tasks/publish-ghPages.js
@@ -1,11 +1,10 @@
 const gulp = require('gulp'),
       ghpages = require('gh-pages'),
       path = require('path'),
-      config = require('../../configuration/config'),
-      build = require('../../gulpfile.js');
-//publish to github
+      config = require('../../configuration/config');
 
-gulp.task('publish-gh', gulp.series('build', function(done) {
+//publish to github
+gulp.task('publish-gh', gulp.series('clean','build', function(done) {
     if (process.env.NODE_ENV != 'production') {throw new Error("gulp publish-gh needs to have NODE_ENV = 'production'. It is best to invoke it with 'npm run publish-gh' instead!");}//console.error();}
     ghpages.publish(config.dir.dest, {
     repo: config.publish.ghPagesRepo,
